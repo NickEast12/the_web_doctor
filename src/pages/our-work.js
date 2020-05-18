e68569d8c4a10cb4a8f180b0903825f84d9871e9
NickEast12 <nickeast1998@gmail.com>
2020-05-18 19:28:44 +0100

    Final content update

--- src/pages/our-work.js
+++ src/pages/our-work.js
@@ -1,33 +1,35 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
 
 import Headertitle from "../components/header-title"
-import Placeholder from "../images/islandP1.jpg"
+import Island from "../images/islandP1.jpg"
+import Adventure from "../images/adv1.jpg"
+import Festival from "../images/boom4.jpg"
+import Coffee from "../images/coffee1.jpg"
+import Car from "../images/tesla.jpg"
+import Villa from "../images/villa.jpg"
+const ImageBoxes = styled.div``
 
-const ImageBoxes = styled.div`
-  div {
-    &:nth-child(1) {
-      background-image: url(${Placeholder});
-    }
-    &:nth-child(2) {
-      background-image: url(${Placeholder});
-    }
-    &:nth-child(3) {
-      background-image: url(${Placeholder});
-    }
-    &:nth-child(4) {
-      background-image: url(${Placeholder});
-    }
-    &:nth-child(5) {
-      background-image: url(${Placeholder});
-    }
-    &:nth-child(6) {
-      background-image: url(${Placeholder});
-    }
-  }
+const Box1 = styled.div`
+  background-image: url(${Island});
+`
+const Box2 = styled.div`
+  background-image: url(${Adventure});
+`
+const Box3 = styled.div`
+  background-image: url(${Festival});
+`
+const Box4 = styled.div`
+  background-image: url(${Coffee});
+`
+const Box5 = styled.div`
+  background-image: url(${Car});
+`
+const Box6 = styled.div`
+  background-image: url(${Villa});
 `
 
 const ContactPage = () => (
@@ -36,24 +38,36 @@ const ContactPage = () => (
     <div className="work__wrapper">
       <Headertitle title="We ensure all we do had" span=" form & function" />
       <ImageBoxes className="work__wrapper__lower">
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 1</h4>
-        </div>
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 2</h4>
-        </div>
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 3</h4>
-        </div>
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 4</h4>
-        </div>
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 5</h4>
-        </div>
-        <div className="work__wrapper__lower__boxes">
-          <h4>Project 6</h4>
-        </div>
+        <Link to="/projects/luxury-island">
+          <Box1 className="work__wrapper__lower__boxes">
+            <h4>Luxury Island</h4>
+          </Box1>
+        </Link>
+        <Link to="/projects/adventure-hiking">
+          <Box2 className="work__wrapper__lower__boxes">
+            <h4>Adventure Hiking</h4>
+          </Box2>
+        </Link>
+        <Link to="/projects/euphoria-festival">
+          <Box3 className="work__wrapper__lower__boxes">
+            <h4>Euphoria Festival</h4>
+          </Box3>
+        </Link>
+        <Link to="/projects/independent-coffee">
+          <Box4 className="work__wrapper__lower__boxes">
+            <h4>Independent Coffee</h4>
+          </Box4>
+        </Link>
+        <Link to="/">
+          <Box5 className="work__wrapper__lower__boxes">
+            <h4>ALSET</h4>
+          </Box5>
+        </Link>
+        <Link to="/">
+          <Box6 className="work__wrapper__lower__boxes">
+            <h4>Prime Villas</h4>
+          </Box6>
+        </Link>
       </ImageBoxes>
     </div>
   </Layout>
